a70bd605f2bc08b54f682d26a6f20d429c19b552
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-10-19 20:19:51 +0200

    JS y refactor del cuestionario

--- SwingCr/src/main.js
+++ SwingCr/src/main.js
@@ -1,1 +1,14 @@
 import "./style.css";
+
+const seleccion = document.getElementById("tipo-evento");
+const campoClase = document.getElementById("campos-clase");
+const campoActividades = document.getElementById("campos-actividades");
+
+seleccion.addEventListener("change", () => {
+  const valor = seleccion.value;
+  switch (valor){
+    case "clase":
+        break;
+    case "actividad"
+  }
+});
