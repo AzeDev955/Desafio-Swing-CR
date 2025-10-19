023c3bb25234ddeca8f36e37cc94cd0484f17bff
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-10-19 20:43:13 +0200

    commit

--- SwingCr/src/main.js
+++ SwingCr/src/main.js
@@ -1,14 +1,35 @@
 import "./style.css";
 
-const seleccion = document.getElementById("tipo-evento");
+const seleccionActividad = document.getElementById("tipo-evento");
 const campoClase = document.getElementById("campos-clase");
-const campoActividades = document.getElementById("campos-actividades");
+const campoActividades = document.getElementById("campos-actividad");
 
-seleccion.addEventListener("change", () => {
-  const valor = seleccion.value;
-  switch (valor){
-    case "clase":
-        break;
-    case "actividad"
+seleccionActividad.addEventListener("change", () => {
+  const valor = seleccionActividad.value;
+  switch (valor) {
+    case "Clase":
+      campoClase.classList.remove("oculto");
+      campoActividades.classList.add("oculto");
+      break;
+    case "Actividad":
+      campoClase.classList.add("oculto");
+      campoActividades.classList.remove("oculto");
+      break;
+
+    default:
+      campoClase.classList.add("oculto");
+      campoActividades.classList.add("oculto");
+      break;
+  }
+});
+
+const seleccionBanda = document.getElementById("actividad-tipo");
+const campoBanda = document.getElementById("grupo-banda");
+
+seleccionBanda.addEventListener("change", () => {
+  if (seleccionBanda.value === "Concierto") {
+    campoBanda.classList.remove("oculto");
+  } else {
+    campoBanda.classList.add("oculto");
   }
 });
