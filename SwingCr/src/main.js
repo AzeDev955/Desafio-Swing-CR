98ed3d41e5f3956e45e69a4916bf921e48d8b6a9
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-10-21 01:29:01 +0200

    Update main.js
    primer try

--- SwingCr/src/main.js
+++ SwingCr/src/main.js
@@ -32,196 +32,203 @@ const horasActividades = [
   "23:00",
   "00:00",
 ];
-
+const formulario = document.getElementById("formulario-registro");
 const tablaClases = document.getElementById("tabla-clases");
-let cuerpoClase;
+
 if (tablaClases) {
-  cuerpoClase = tablaClases.querySelector("tbody");
+  let cuerpoClase;
+  if (tablaClases) {
+    cuerpoClase = tablaClases.querySelector("tbody");
+  }
+
+  const tablaActividades = document.getElementById("tabla-actividades");
+
+  let cuerpoActividades;
+  if (tablaActividades) {
+    cuerpoActividades = tablaActividades.querySelector("tbody");
+  }
+
+  horasClase.forEach((hora) => {
+    let nuevaFila = document.createElement("tr");
+    let nuevoTh = document.createElement("th");
+    nuevoTh.textContent = hora;
+    nuevaFila.appendChild(nuevoTh);
+    for (let i = 0; i < 3; i++) {
+      let nuevoTd = document.createElement("td");
+      nuevaFila.appendChild(nuevoTd);
+    }
+    cuerpoClase.appendChild(nuevaFila);
+  });
+
+  horasActividades.forEach((hora) => {
+    let nuevaFila = document.createElement("tr");
+    let nuevoTh = document.createElement("th");
+    nuevoTh.textContent = hora;
+    nuevaFila.appendChild(nuevoTh);
+    for (let i = 0; i < 3; i++) {
+      let nuevoTd = document.createElement("td");
+      nuevaFila.appendChild(nuevoTd);
+    }
+    cuerpoActividades.appendChild(nuevaFila);
+  });
+  const cargarEventos = () => {
+    const eventosGuardados = localStorage.getItem(STORAGE);
+    if (eventosGuardados) {
+      return JSON.parse(eventosGuardados);
+    } else {
+      return [];
+    }
+  };
+
+  const listaEventos = cargarEventos();
+  const soloClases = [];
+  const soloActividades = [];
+
+  listaEventos.forEach((evento) => {
+    if (evento instanceof Clase) {
+      soloClases.push(evento);
+    } else {
+      soloActividades.push(evento);
+    }
+  });
+
+  soloClases.forEach((clase) => {
+    let tarjeta = document.createElement("div");
+    tarjeta.classList.add("tarjeta-evento");
+
+    let tituloTarjeta = document.createElement("div");
+    tituloTarjeta.classList.add("tarjeta-evento__titulo");
+    tituloTarjeta.textContent = clase.estilo;
+
+    let ubicacionTarjeta = document.createElement("div");
+    ubicacionTarjeta.classList.add("tarjeta-evento__ubicacion");
+    ubicacionTarjeta.textContent = clase.ubicacion;
+
+    tarjeta.appendChild(tituloTarjeta);
+    tarjeta.appendChild(ubicacionTarjeta);
+
+    const filaCorrecta = cuerpoClase.querySelector(
+      `tr th:first-child[textContent='${clase.hora}']`
+    ); //con ayuda de nuestro amigo confiable
+
+    if (filaCorrecta) {
+      let columna;
+      switch (clase.dia) {
+        case "Viernes":
+          columna = 0;
+          break;
+        case "Sabado":
+          columna = 1;
+          break;
+        case "Domingo":
+          columna = 2;
+          break;
+      }
+      const celdas = filaCorrecta.querySelectorAll("td");
+      const celdaUbicacion = celdas[columna];
+      celdaUbicacion.appendChild(tarjeta);
+    }
+  });
 }
 
-const tablaActividades = document.getElementById("tabla-actividades");
+///////////////////////////////////////////////////////////////////////
+else if (formulario) {
+  const salasClase = ["Be Hopper", "New Orleans", "Savoy"];
+  const salasActividades = ["Antiguo casino", "Parque", "Prado"];
 
-let cuerpoActividades;
-if (tablaActividades) {
-  cuerpoActividades = tablaActividades.querySelector("tbody");
-}
+  const seleccionActividad = document.getElementById("tipo-evento");
+  const campoClase = document.getElementById("campos-clase");
+  const campoActividades = document.getElementById("campos-actividad");
+  const campoUbicacionesClases = document.getElementById("ubicaciones-clases");
+  const campoUbicacionesActividades = document.getElementById(
+    "ubicaciones-actividades"
+  );
 
-horasClase.forEach((hora) => {
-  let nuevaFila = document.createElement("tr");
-  let nuevoTh = document.createElement("th");
-  nuevoTh.textContent = hora;
-  nuevaFila.appendChild(nuevoTh);
-  for (let i = 0; i < 3; i++) {
-    let nuevoTd = document.createElement("td");
-    nuevaFila.appendChild(nuevoTd);
-  }
-  cuerpoClase.appendChild(nuevaFila);
-});
-
-horasActividades.forEach((hora) => {
-  let nuevaFila = document.createElement("tr");
-  let nuevoTh = document.createElement("th");
-  nuevoTh.textContent = hora;
-  nuevaFila.appendChild(nuevoTh);
-  for (let i = 0; i < 3; i++) {
-    let nuevoTd = document.createElement("td");
-    nuevaFila.appendChild(nuevoTd);
-  }
-  cuerpoActividades.appendChild(nuevaFila);
-});
-const cargarEventos = () => {
-  const eventosGuardados = localStorage.getItem(STORAGE);
-  if (eventosGuardados) {
-    return JSON.parse(eventosGuardados);
-  } else {
-    return [];
-  }
-};
-
-const listaEventos = cargarEventos();
-const soloClases = [];
-const soloActividades = [];
-
-listaEventos.forEach((evento) => {
-  if (evento instanceof Clase) {
-    soloClases.push(evento);
-  } else {
-    soloActividades.push(evento);
-  }
-});
-
-soloClases.forEach((clase) => {
-  let tarjeta = document.createElement("div");
-  tarjeta.classList.add("tarjeta-evento");
-
-  let tituloTarjeta = document.createElement("div");
-  tituloTarjeta.classList.add("tarjeta-evento__titulo");
-  tituloTarjeta.textContent = clase.estilo;
-
-  let ubicacionTarjeta = document.createElement("div");
-  ubicacionTarjeta.classList.add("tarjeta-evento__ubicacion");
-  ubicacionTarjeta.textContent = clase.ubicacion;
-
-  tarjeta.appendChild(tituloTarjeta);
-  tarjeta.appendChild(ubicacionTarjeta);
-
-  const filaCorrecta = cuerpoClase.querySelector(
-    `tr th:first-child[textContent='${clase.hora}']`
-  ); //con ayuda de nuestro amigo confiable
-
-  if (filaCorrecta) {
-    let columna;
-    switch (clase.dia) {
-      case "Viernes":
-        columna = 0;
+  seleccionActividad.addEventListener("change", () => {
+    const valor = seleccionActividad.value;
+    switch (valor) {
+      case "Clase":
+        campoClase.classList.remove("oculto");
+        campoActividades.classList.add("oculto");
+        campoUbicacionesClases.classList.remove("oculto");
+        campoUbicacionesActividades.classList.add("oculto");
         break;
-      case "Sabado":
-        columna = 1;
+      case "Actividad":
+        campoClase.classList.add("oculto");
+        campoActividades.classList.remove("oculto");
+        campoUbicacionesClases.classList.remove("oculto");
+        campoUbicacionesActividades.classList.remove("oculto");
         break;
-      case "Domingo":
-        columna = 2;
+
+      default:
+        campoClase.classList.add("oculto");
+        campoActividades.classList.add("oculto");
+        campoUbicacionesClases.classList.add("oculto");
+        campoUbicacionesActividades.classList.add("oculto");
         break;
     }
-    const celdas = filaCorrecta.querySelectorAll("td");
-    const celdaUbicacion = celdas[columna];
-    celdaUbicacion.appendChild(tarjeta);
-  }
-});
+  });
 
-const salasClase = ["Be Hopper", "New Orleans", "Savoy"];
-const salasActividades = ["Antiguo casino", "Parque", "Prado"];
+  const seleccionBanda = document.getElementById("actividad-tipo");
+  const campoBanda = document.getElementById("grupo-banda");
 
-const formulario = document.getElementById("formulario-registro");
-const seleccionActividad = document.getElementById("tipo-evento");
-const campoClase = document.getElementById("campos-clase");
-const campoActividades = document.getElementById("campos-actividad");
-const campoUbicacionesClases = document.getElementById("ubicaciones-clases");
-const campoUbicacionesActividades = document.getElementById(
-  "ubicaciones-actividades"
-);
+  seleccionBanda.addEventListener("change", () => {
+    if (seleccionBanda.value === "Concierto") {
+      campoBanda.classList.remove("oculto");
+    } else {
+      campoBanda.classList.add("oculto");
+    }
+  });
 
-seleccionActividad.addEventListener("change", () => {
-  const valor = seleccionActividad.value;
-  switch (valor) {
-    case "Clase":
-      campoClase.classList.remove("oculto");
-      campoActividades.classList.add("oculto");
-      campoUbicacionesClases.classList.remove("oculto");
-      campoUbicacionesActividades.classList.add("oculto");
-      break;
-    case "Actividad":
-      campoClase.classList.add("oculto");
-      campoActividades.classList.remove("oculto");
-      campoUbicacionesClases.classList.remove("oculto");
-      campoUbicacionesActividades.classList.remove("oculto");
-      break;
+  formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const horaOption = document.getElementById("hora");
+    const hora = horaOption.value;
 
-    default:
-      campoClase.classList.add("oculto");
-      campoActividades.classList.add("oculto");
-      campoUbicacionesClases.classList.add("oculto");
-      campoUbicacionesActividades.classList.add("oculto");
-      break;
-  }
-});
+    const diaOption = document.getElementById("dia");
+    const dia = diaOption.value;
 
-const seleccionBanda = document.getElementById("actividad-tipo");
-const campoBanda = document.getElementById("grupo-banda");
+    const ubicacionOption = document.querySelector(
+      "input[name='ubicacion']:checked"
+    );
+    let ubicacion = "";
+    if (ubicacionOption) {
+      ubicacion = ubicacionOption.value;
+    }
+    const actividad = seleccionActividad.value;
+    let evento;
+    switch (actividad) {
+      case "Clase":
+        const estiloOption = document.getElementById("clase-estilo");
+        const nivelOption = document.getElementById("clase-nivel");
+        const profesorInput = document.getElementById("clase-profesores");
 
-seleccionBanda.addEventListener("change", () => {
-  if (seleccionBanda.value === "Concierto") {
-    campoBanda.classList.remove("oculto");
-  } else {
-    campoBanda.classList.add("oculto");
-  }
-});
+        const profesor = profesorInput.value;
+        const nivel = nivelOption.value;
+        const estilo = estiloOption.value;
 
-formulario.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const horaOption = document.getElementById("hora");
-  const hora = horaOption.value;
+        evento = new Clase(dia, hora, ubicacion, estilo, nivel, profesor);
+        break;
+      case "Actividad":
+        const tipoOption = document.getElementById("actividad-tipo");
+        const tipo = tipoOption.value;
 
-  const diaOption = document.getElementById("dia");
-  const dia = diaOption.value;
+        let banda = "";
+        if (tipo === "Concierto") {
+          const bandaInput = document.getElementById("actividad-banda");
+          banda = bandaInput.value;
+        }
 
-  const ubicacionOption = document.querySelector(
-    "input[name='ubicacion']:checked"
-  );
-  let ubicacion = "";
-  if (ubicacionOption) {
-    ubicacion = ubicacionOption.value;
-  }
-  const actividad = seleccionActividad.value;
-  let evento;
-  switch (actividad) {
-    case "Clase":
-      const estiloOption = document.getElementById("clase-estilo");
-      const nivelOption = document.getElementById("clase-nivel");
-      const profesorInput = document.getElementById("clase-profesores");
+        const descripcionInput = document.getElementById(
+          "actividad-descripcion"
+        );
+        const descripcion = descripcionInput.value;
 
-      const profesor = profesorInput.value;
-      const nivel = nivelOption.value;
-      const estilo = estiloOption.value;
-
-      evento = new Clase(dia, hora, ubicacion, estilo, nivel, profesor);
-      break;
-    case "Actividad":
-      const tipoOption = document.getElementById("actividad-tipo");
-      const tipo = tipoOption.value;
-
-      let banda = "";
-      if (tipo === "Concierto") {
-        const bandaInput = document.getElementById("actividad-banda");
-        banda = bandaInput.value;
-      }
-
-      const descripcionInput = document.getElementById("actividad-descripcion");
-      const descripcion = descripcionInput.value;
-
-      evento = new Actividad(dia, hora, ubicacion, tipo, banda, descripcion);
-      break;
-  }
-  listaEventos.push(evento);
-  const eventosJSON = JSON.stringify(listaEventos);
-  localStorage.setItem(STORAGE, eventosJSON);
-});
+        evento = new Actividad(dia, hora, ubicacion, tipo, banda, descripcion);
+        break;
+    }
+    listaEventos.push(evento);
+    const eventosJSON = JSON.stringify(listaEventos);
+    localStorage.setItem(STORAGE, eventosJSON);
+  });
+}
